0391d9a0e4baca4b27ee6880e3af168aba7094bb
Vladimir Semenov <svowl05@gmail.com>
2023-01-23 09:38:43 +0000

    Added description of Variables interface in scripts.om.d.ts

--- API/scripts.om.d.ts
+++ API/scripts.om.d.ts
@@ -1862,6 +1862,16 @@ export namespace Notifications {
     }
 }
 
+export interface Variable {
+    isEntity(): boolean;
+
+    getValue(): number | string | null | boolean | EntityInfo;
+}
+
+export interface Variables {
+    get(varName: string): Variable;
+}
+
 export interface OM {
     readonly common: Common;
     readonly environment: Environment;
@@ -1873,6 +1883,7 @@ export interface OM {
     readonly optimization: Optimization;
     readonly connectors: Connectors;
     readonly notifications: Notifications.Manager;
+    readonly variables: Variables;
 }
 
 export var om: OM;
